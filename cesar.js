ce5c8b651a48d7fd9637445708132a8dd1db0df3
joana1818 <161776671+joana1818@users.noreply.github.com>
2024-11-19 20:11:49 -0300

    Add files via upload

---
+++ cesar.js
@@ -0,0 +1,21 @@
+function criptografia(mensagem,chave,converter){
+    return converter(letra,chave);
+ 
+}
+
+function cifraLetra(letra,chave){
+    const alfabeto = "abcdefghij"
+    for(i=0; i<alfabeto.length;i++){
+    
+    }
+
+}
+
+function decifraLetra(letra,chave){
+    const alfabeto = "abcdefghij"
+    for(i=0; i>alfabeto.length;i++){
+        
+    }
+}
+
+export function criptografia
---
+++ index.js
@@ -0,0 +1,3 @@
+import calc from './calculadora.js'
+
+console.log(calc.soma(3,4))
