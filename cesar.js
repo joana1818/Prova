d0336524ab702990e014fd3587d4ef34bec50acb
joana1818 <161776671+joana1818@users.noreply.github.com>
2024-11-19 23:01:19 -0300

    Update cesar.js

--- cesar.js
+++ cesar.js
@@ -1,21 +1,43 @@
-function criptografia(mensagem,chave,converter){
-    return converter(letra,chave);
- 
-}
+criptografia = (mensagem, callback) => {
+    let resultado = '';
+    for (let i = 0; i < mensagem.length; i++) {
+        resultado += callback(mensagem[i], 3); 
+    }
+    return resultado;
+};
+
+function converter = (letra, chave) => {
+    const alfabeto = '';
+    for (let i = 0; i < alfabeto.length; i++) {
+        if (letra = alfabeto[i]) {
+            return alfabeto(i + chave);
+        }
+    }
+    return letra;
+};
 
 function cifraLetra(letra,chave){
-    const alfabeto = "abcdefghij"
+    const alfabeto = '';
     for(i=0; i<alfabeto.length;i++){
+         let letra = alfabeto[i]{
+            return alfabeto(i + chave);
+        
+        }
+        return letra;  
     
     }
 
-}
+};
 
 function decifraLetra(letra,chave){
-    const alfabeto = "abcdefghij"
-    for(i=0; i>alfabeto.length;i++){
+    const alfabeto = '';
+    for(i=0; i<alfabeto.length;i++){
+        let letra = alfabeto[i];{
+            return alfabeto(i - chave);
+        }
+        return letra;
         
     }
-}
+};
 
-export function criptografia
+module exports = {criptografia,cifraLetra,decifraLetra,criptografia};
